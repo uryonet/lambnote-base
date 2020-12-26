7c48b9c2cce7aaa8981aaacb0c78a0b606273955
ryo yoneyama <uryonet@gmail.com>
2020-12-26 22:11:07 +0900

    #8 セクション作成ボタンと新規セクション作成処理の追加

--- src/features/sections/SectionsList.tsx
+++ src/features/sections/SectionsList.tsx
@@ -1,19 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectNote } from 'features/notes/noteSlice'
-import { fetchSectionsData, selectSections } from 'features/sections/sectionsSlice'
+import { createNewSection, fetchSectionsData, selectSections } from 'features/sections/sectionsSlice'
 import { fetchPagesData } from '../pages/pagesSlice'
 
 export const SectionsList: React.FC = () => {
   const dispatch = useDispatch()
   const { lambnoteId } = useSelector(selectNote)
   const { sections } = useSelector(selectSections)
+  const [sectionName, setSectionName] = useState('')
 
   useEffect(() => {
     if (lambnoteId) {
       dispatch(fetchSectionsData(lambnoteId))
     }
   }, [lambnoteId])
+
+  const onChangeNewSection = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSectionName(event.target.value)
+  }
+
+  const handleCreateSection = () => {
+    dispatch(createNewSection(lambnoteId, sectionName))
+    setSectionName('')
+  }
 
   const handleSection = (id: string | undefined) => {
     if (id) {
@@ -24,6 +34,10 @@ export const SectionsList: React.FC = () => {
   return (
     <div className="sections-list">
       <h2>セクション</h2>
+      <div className="create-section">
+        <input value={sectionName} onChange={onChangeNewSection} />
+        <button onClick={handleCreateSection}>セクション作成</button>
+      </div>
       <ul>
         {sections.map(({ id, displayName }) => {
           return (
