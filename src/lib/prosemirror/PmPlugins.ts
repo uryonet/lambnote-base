a94fb08fd38ba6ded8133f70f67b5e90d559f71a
ryo yoneyama <uryonet@gmail.com>
2021-01-03 06:47:10 +0900

    #23 マークダウン入力ルールにリストとコードブロックを追加

--- src/lib/prosemirror/PmPlugins.ts
+++ src/lib/prosemirror/PmPlugins.ts
@@ -1,27 +1,16 @@
-import { Plugin, PluginKey } from 'prosemirror-state'
+import { Plugin } from 'prosemirror-state'
 import { undo, redo, history } from 'prosemirror-history'
 import { keymap } from 'prosemirror-keymap'
 import { baseKeymap } from 'prosemirror-commands'
+import { Schema } from 'prosemirror-model'
 import markdownInputRules from './markdownInputRules'
 
-export const pmPlugins = (): Plugin[] => {
-  const addClassPlugin = new Plugin({
-    props: {
-      attributes: { class: 'zeronote-editor' }
-    }
-  })
-
+export const pmPlugins = (schema: Schema): Plugin[] => {
   return [
     history(),
     keymap({ 'Mod-z': undo, 'Mod-y': redo }),
     keymap(baseKeymap),
-    setPluginKey(markdownInputRules(), 'InputRules')
+    markdownInputRules(schema)
     // addClassPlugin
   ]
-}
-
-const setPluginKey = (plugin: any, key: string) => {
-  plugin.spec.key = new PluginKey(key + 'Plugin')
-  plugin.key = plugin.spec.key.key
-  return plugin
 }
