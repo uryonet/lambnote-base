90eaafc2687531eef58c75b00d5ec29b38f49fa9
ryo yoneyama <uryonet@gmail.com>
2021-01-24 13:44:25 +0900

    #40 spinnerの表示処理を削除

--- src/features/pages/PagesListItems.tsx
+++ src/features/pages/PagesListItems.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchPageData, selectPages, deletePage } from './pagesSlice'
-import { selectSections } from '../sections/sectionsSlice'
-
-import Spinner from 'react-bootstrap/Spinner'
+import { fetchPageData, selectPages } from './pagesSlice'
 
 export const PagesListItem: React.FC = () => {
   const dispatch = useDispatch()
-  const { currentSectionId } = useSelector(selectSections)
-  const { isLoading, pages } = useSelector(selectPages)
+  const { pages, currentPageId } = useSelector(selectPages)
 
   const handlePage = (id: string | undefined) => {
     if (id) {
@@ -16,25 +12,17 @@ export const PagesListItem: React.FC = () => {
     }
   }
 
-  if (isLoading) {
-    return (
-      <div className="load-spinner">
-        <Spinner animation="border" />
-      </div>
-    )
-  } else {
-    return (
-      <ul>
-        {pages.map(({ id, title }) => {
-          return (
-            <li key={id} className={id === currentSectionId ? 'selected' : ''}>
-              <a href="#" onClick={() => handlePage(id)}>
-                {title}
-              </a>
-            </li>
-          )
-        })}
-      </ul>
-    )
-  }
+  return (
+    <ul>
+      {pages.map(({ id, title }) => {
+        return (
+          <li key={id} className={id === currentPageId ? 'selected' : ''}>
+            <a href="#" onClick={() => handlePage(id)}>
+              {title}
+            </a>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
