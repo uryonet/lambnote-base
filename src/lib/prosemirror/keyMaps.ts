a94d77495b6ca3fae53694e5cc4a839c454e0197
ryo yoneyama <uryonet@gmail.com>
2021-01-31 21:49:33 +0900

    # 26 example-setupとキーマップ設定を合わせていろいり追加

--- src/lib/prosemirror/keyMaps.ts
+++ src/lib/prosemirror/keyMaps.ts
@@ -1,4 +1,4 @@
-import { Keymap, toggleMark } from 'prosemirror-commands'
+import { chainCommands, exitCode, Keymap, setBlockType, toggleMark } from 'prosemirror-commands'
 import { undo, redo } from 'prosemirror-history'
 import { splitListItem } from 'prosemirror-schema-list'
 import { undoInputRule } from 'prosemirror-inputrules'
@@ -14,6 +14,25 @@ export const keyMaps = (): Keymap => {
   keys['Mod-B'] = toggleMark(schema.marks.strong)
   keys['Mod-i'] = toggleMark(schema.marks.em)
   keys['Mod-I'] = toggleMark(schema.marks.em)
+  keys['Mod-`'] = toggleMark(schema.marks.code)
+  keys['Shift-Ctrl-0'] = setBlockType(schema.nodes.paragraph)
+  keys['Shift-Ctrl-\\'] = setBlockType(schema.nodes.code_block)
+  keys['Shift-Ctrl-1'] = setBlockType(schema.nodes.heading, { level: 1 })
+  keys['Shift-Ctrl-2'] = setBlockType(schema.nodes.heading, { level: 2 })
+  keys['Shift-Ctrl-3'] = setBlockType(schema.nodes.heading, { level: 3 })
+  keys['Shift-Ctrl-4'] = setBlockType(schema.nodes.heading, { level: 4 })
+  keys['Shift-Ctrl-5'] = setBlockType(schema.nodes.heading, { level: 5 })
+  keys['Shift-Ctrl-6'] = setBlockType(schema.nodes.heading, { level: 6 })
+  let cmd = chainCommands(exitCode, (state, dispatch) => {
+    if (dispatch) {
+      dispatch(state.tr.replaceSelectionWith(schema.nodes.hard_break.create()).scrollIntoView())
+      return true
+    }
+    return false
+  })
+  keys['Mod-Enter'] = cmd
+  keys['Shift-Enter'] = cmd
+  keys['Ctrl-Enter'] = cmd
 
   keys['Enter'] = splitListItem(schema.nodes.list_item)
 
