295c9c443ea4743bec907bb5cda68f1ef8a9ba88
ryo yoneyama <uryonet@gmail.com>
2021-01-31 11:22:50 +0900

    #26 太字と斜線処理を追加

--- src/lib/prosemirror/keyMaps.ts
+++ src/lib/prosemirror/keyMaps.ts
@@ -1,4 +1,4 @@
-import { Keymap } from 'prosemirror-commands'
+import { Keymap, toggleMark } from 'prosemirror-commands'
 import { undo, redo } from 'prosemirror-history'
 import { splitListItem } from 'prosemirror-schema-list'
 import schema from './schema'
@@ -7,6 +7,10 @@ export const keyMaps = (): Keymap => {
   let keys: Keymap = {}
   keys['Mod-z'] = undo
   keys['Mod-y'] = redo
+  keys['Mod-b'] = toggleMark(schema.marks.strong)
+  keys['Mod-B'] = toggleMark(schema.marks.strong)
+  keys['Mod-i'] = toggleMark(schema.marks.em)
+  keys['Mod-I'] = toggleMark(schema.marks.em)
   keys['Enter'] = splitListItem(schema.nodes.list_item)
 
   return keys
