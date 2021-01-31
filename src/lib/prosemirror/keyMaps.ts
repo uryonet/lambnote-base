81391c3e699f589172b80112cd7bae1f373ed9e2
ryo yoneyama <uryonet@gmail.com>
2021-01-31 11:32:46 +0900

    #26 undoInputRuleの追加

--- src/lib/prosemirror/keyMaps.ts
+++ src/lib/prosemirror/keyMaps.ts
@@ -1,16 +1,20 @@
 import { Keymap, toggleMark } from 'prosemirror-commands'
 import { undo, redo } from 'prosemirror-history'
 import { splitListItem } from 'prosemirror-schema-list'
+import { undoInputRule } from 'prosemirror-inputrules'
 import schema from './schema'
 
 export const keyMaps = (): Keymap => {
   let keys: Keymap = {}
   keys['Mod-z'] = undo
   keys['Mod-y'] = redo
+  keys['Backspace'] = undoInputRule
+
   keys['Mod-b'] = toggleMark(schema.marks.strong)
   keys['Mod-B'] = toggleMark(schema.marks.strong)
   keys['Mod-i'] = toggleMark(schema.marks.em)
   keys['Mod-I'] = toggleMark(schema.marks.em)
+
   keys['Enter'] = splitListItem(schema.nodes.list_item)
 
   return keys
